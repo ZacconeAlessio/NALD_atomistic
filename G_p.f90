e58010574293c523e0ff69f6b033a0a00ca05e13
Ankit Singh <ankit.singh@unimi.it>
2026-05-13 16:26:59 +0200

    G_p.f90

--- G_p.f90
+++ G_p.f90
@@ -1,10 +1,10 @@
-Program G
+Program G   ! Fotran codes for integration
 implicit none
-  integer, parameter :: n=3*10074
+  integer, parameter :: n=3*10074   ! N=10074 Number of monomer
   integer, parameter :: m=3*10074
-  integer, parameter :: T=300
+  integer, parameter :: T=300  ! Temperature
   integer, parameter :: samp=2
-  real(8), parameter :: tu=1e+12
+  real(8), parameter :: tu=1e+12 ! frequency in THz 
   real(8), dimension(m) :: v, rk, Gx,Gy,Gz
   real(8), dimension(m) :: swp,wp
   real(8), dimension(m) :: gamma
@@ -14,7 +14,7 @@ implicit none
 
 
 rk=0
-   do tej=1,4
+   do tej=1,4  !trejectories started 1 to 4 runs
 
    write(F0,'(a,I1,a,I1,a)')'../T300/runs/run',samp,'/data/',tej,'/GA.dat'
 open(unit=8,file=F0,status="old")
@@ -28,14 +28,14 @@ write(F6,'(a,I1,a,I1,a,I3,a)')'../T300/runs/run',samp,'/data/',tej,'/G_prime_T',
 open(unit=13,file=F6,status="unknown")
 
 do k=1,1
-read(9,*) v(k)
-read(8,*) Gx(k),Gy(k),Gz(k)
+read(9,*) v(k)  ! volume
+read(8,*) Gx(k),Gy(k),Gz(k)  !Affine modulus
 end do
 
 
 do i=1,m
-read(10,*) swp(i)
-read(111,*) gamma(i)
+read(10,*) swp(i)    !eigenfrequency in term of omega^2
+read(111,*) gamma(i) !Affine force field correlator
 end do
 !++++++++++++++++++++++++++++
 
@@ -48,27 +48,27 @@ end if
 end do
 !++++++++++++++++++++++++++++
 do p=1,235
-w=(tu)*exp(-15+0.1d0*p)
+w=(tu)*exp(-15+0.1d0*p) !External frequency in term of real unit
 
-nu=5e+13 ! Kg/s
+nu=5e+13 ! Kg/s  ! Kg/s friction kernal
 constant=(1e+30)*v(1)**(-1)
-GA= (101325*(Gx(1)+Gy(1)+Gz(1))/3)
+GA= (101325*(Gx(1)+Gy(1)+Gz(1))/3)  ! Affine modulus
 
 sum=0.d0
 do i=1,m
-if(abs(swp(i)).lt.+0.00001d0) then
+if(abs(swp(i)).lt.+0.00001d0) then  !Avoid Goldstone modes
 gamma(i)=0.d0
-elseif((swp(i)).gt.-2.570d0.and.(swp(i)).lt.+2.570d0) then 
+elseif((swp(i)).gt.-2.570d0.and.(swp(i)).lt.+2.570d0) then  ! Discart Unphysical modes belwo cutoff frequency or qudratic fit
 gamma(i)=0!0.1157d0*abs(swp(i))
 else
 gamma(i)=gamma(i)
 endif
-f=-2.906*(1e+6)*((tu**2)*swp(i)-w*w)*gamma(i)*(((tu**2)*swp(i)-w*w)**(+2)+1*nu*nu*w*w)**(-1)
+f=-2.906*(1e+6)*((tu**2)*swp(i)-w*w)*gamma(i)*(((tu**2)*swp(i)-w*w)**(+2)+1*nu*nu*w*w)**(-1)  !Integration perform in term of sum and in proper SI unit
 sum=sum+f
 end do
-write(13,*)w*(tu**(-1)), (1*(GA)+sum*constant)*(1e-9)
+write(13,*)w*(tu**(-1)), (1*(GA)+sum*constant)*(1e-9)  !modulus results as a function of external frequency
 end do
-write(*,15) (101325*(Gx(1)+Gy(1)+Gz(1))/3)
+write(*,15) (101325*(Gx(1)+Gy(1)+Gz(1))/3) ! write on screen to check
 end do
 14 format(2I7,15f10.2)
 15 format(2e15.7)
